c6d233e3cb722e4a3d267233641837228cea9eff
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-07-06 12:44:54 -0500

    Finished part 10 of Tutorial.

--- angularjs/angular-phonecat/app/js/controllers.js
+++ angularjs/angular-phonecat/app/js/controllers.js
@@ -12,8 +12,14 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http', function($sc
   $scope.orderProp = 'age';
 }]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+function($scope, $routeParams, $http) {
   $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
     $scope.phone = data;
+    $scope.mainImageUrl = data.images[0];
   });
+
+  $scope.setImage = function(imageUrl) {
+    $scope.mainImageUrl = imageUrl;
+  }
 }]);

8f71209b9ea19f54acf3142e76fabec807be7017
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-07-16 18:44:35 -0500

    Add UsersController and new action

---
+++ rails/sample_app/spec/features/user_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature "UserPages", :type => :feature do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+
+end
